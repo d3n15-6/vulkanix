6569d267cf8c00ceaf8ee1004e83f17b1af693de
d3n15-6 <che.hae.denis@gmail.com>
2026-09-02 19:56:03 -0300

    feat(math): add common direction constants to Vector3
    Adds UP, DOWN, RIGHT, LEFT, FORWARD and BACKWARD constants for easier spacial positioning.

--- crates/vulkanix-math/src/vector3.rs
+++ crates/vulkanix-math/src/vector3.rs
@@ -14,6 +14,12 @@ impl Vector3 {
     pub const ZERO: Self = Self::new(0.0, 0.0, 0.0);
     pub const ONE: Self = Self::new(1.0, 1.0, 1.0);
 
+    pub const UP: Self = Self::new(0.0, 1.0, 0.0);
+    pub const DOWN: Self = Self::new(0.0, -1.0, 0.0);
+    pub const RIGHT: Self = Self::new(1.0, 0.0, 0.0);
+    pub const LEFT: Self = Self::new(-1.0, 0.0, 0.0);
+    pub const FORWARD: Self = Self::new(0.0, 0.0, -1.0);
+    pub const BACKWARD: Self = Self::new(0.0, 0.0, 1.0);
 
 }
 
